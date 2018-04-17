63b7d39b564574e984667c9250d57a0dfdc4ea68
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-04-17 14:39:18 -0400

    partially working products view

---
+++ client/components/products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Products = () => {
+  return (
+    <div className='products-container'>
+      <div className='products-container-search-bar'>
+        <h1>Search</h1>
+      </div>
+      <div className='test-border-dot'>
+        <p>test</p>
+      </div>
+    </div>
+  );
+};
