fcb7a91a22332865bf386d597ec63a06b93797ce
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-04-17 16:38:31 -0400

    oh god I hope this works

--- client/components/products/MiniProduct.js
+++ client/components/products/MiniProduct.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
 const MiniProduct = (props) => {
-  // const { name, description, price, imageUrl } = props; 
-  const name = "Skyrim",
-        description = "This is a wonderful game set in the province of skyrim. Beware of dragons and bandits",
-        price = "$15.99",
-        imageUrl = "https://cdn3.dualshockers.com/wp-content/uploads/2017/06/tes-skyrim_switch_frontcover-07-RP_1496832137.jpg"
+  const { title, description, price, imageURL } = props.product;
   return (
     <div className='mini-product-container'>
       <div className='mini-product-top-row'>
         <div className='mini-product-container-image'>
-          <img src={imageUrl} alt="test" />
+          <img src={imageURL} alt="test" />
         </div>
         <div className='mini-product-container-name-price'>
-          <h3 id="mini-product-name">{name}</h3>
+          <h3 id="mini-product-name">{title}</h3>
           <h3 id="mini-product-price">{price}</h3>
         </div>
       </div>
--- client/components/products/Products.js
+++ client/components/products/Products.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import MiniProduct from './MiniProduct';
+import {connect} from 'react-redux'
 
-export const Products = () => {
+const Products = (props) => {
   return (
     <div className='products-container'>
       <div id='products-container-search-bar'>
         <h1>Search</h1>
       </div>
       <div className='test-border-dot'>
-        <MiniProduct />
+        {props.products.map((product) => {
+          return <MiniProduct key={product.id} product={product} />
+        })}
       </div>
     </div>
   );
 };
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.products
+  };
+};
+
+export default connect(mapStateToProps, null)(Products);
