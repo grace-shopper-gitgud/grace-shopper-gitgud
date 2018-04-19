e86e720715361e8f79038c6d9df17d881cde84f5
Tim Carleton <carletontimothy@gmail.com>
2018-04-19 12:44:31 -0400

    added review, order model and associations

--- server/db/index.js
+++ server/db/index.js
@@ -2,14 +2,22 @@ const db = require('./database')
 const User = require('./user')
 const Product = require('./product')
 const Category = require('./category')
+const Review = require('./review')
+const Order = require('./order')
+
 // associations go here!
 Product.belongsToMany(Category, {through: 'product-category'})
 Category.belongsToMany(Product, {through: 'product-category'})
+
+Product.belongsToMany(Order, {through: 'product-order'})
+Order.belongsToMany(Product, {through: 'product-order'})
 
 
 module.exports = {
   db,
   User,
   Product,
-  Category
+  Category,
+  Review,
+  Order
 }
