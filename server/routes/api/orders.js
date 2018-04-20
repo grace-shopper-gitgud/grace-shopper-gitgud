c17e6375cef6a71c9a88b9d53d3c05379d3ca734
Tyler Chan <TcFlux@gmail.com>
2018-04-20 16:06:13 -0400

    create express order post route

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
-const {Order} = require('../../db');
+const {Order, User} = require('../../db');
 
 module.exports = router;
 
+router.param('userId', (req, res, next, userId) => {
+  User.findById(userId)
+  .then(user => {
+    if (!user) {
+      return new Error('404 Not Found');
+    }
+    req.user = user;
+    next();
+  })
+  .catch(next);
+})
+
 router.get('/:userId', async (req, res, next) => {
-  const userId = req.params.userId;
   try {
     const orders = await Order.findAll({
       where: {
-        userId
+        userId: req.user.id
       }
     })
     res.json(orders);
@@ -16,3 +27,12 @@ router.get('/:userId', async (req, res, next) => {
     (next(err))
   }
 });
+
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.create(req.body)
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+})
